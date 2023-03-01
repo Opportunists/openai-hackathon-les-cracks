ccd33135b555b38753e5e0f156c2ee955250a85c
Yasha <yassine.damiri@epita.fr>
2023-03-01 03:11:39 +0100

    feat(router): Add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+/* Styles */
 import "./index.css";
 
 import { UseIt } from './pages/UseIt'
+/* Pages */
 import MainPage from './pages/MainPage'
 
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage/>,
+  },
+  {
+    path: "/start",
+    element: <UseIt/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <UseIt/>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
